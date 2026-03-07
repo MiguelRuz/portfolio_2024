8362ffed3134c93dda1c29c36ff94c05ae7fe247
Miguel Ruz <miguelruzvelasco@gmail.com>
2026-03-07 22:32:20 +0100

    ajustes diseño

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
 const images = [
   "images/hero1.jpg",
   "images/hero2.jpg",
-  "images/hero3.jpg"
+  "images/hero3.jpg",
+  "images/hero4.jpg",
+  "images/hero5.jpg"
 ];
 
 let index = 0;
